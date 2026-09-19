54dbc7165bfdd0ed281f4af2e81232773a0f7566
irshad-max <khushbudeenshah@gmail.com>
2026-09-19 17:07:18 +0530

    Update RearrangeArrayBySign.java

--- RearrangeArrayBySign.java
+++ RearrangeArrayBySign.java
@@ -1,27 +1,25 @@
 import java.util.*;
-
-public class RearrangeArrayBySign {
-    public static void main(String[] args) {
-
-        int arr[] = {3, 1, -2, -5, 2, -4};
-        int n = arr.length;
-
-        int result[] = new int[n];
-
-        int pos = 0;
-        int neg = 1;
-
-        for (int num : arr) {
-
-            if (num > 0) {
-                result[pos] = num;
-                pos += 2;
-            } else {
-                result[neg] = num;
-                neg += 2;
+class RearrangeArrayBySign{
+    public static void main(String[] args){
+        int arr[]={3,1,2,-5,-2,-4};
+        //extra space O(n)
+        int ans[]=new int[arr.length];
+        int pos=0;
+        int neg=1;
+        for(int x:arr){
+            //x is positive so add in even
+           // position like 0,2,4
+            if(x>0){
+                ans[pos]=x;
+                pos+=2;
+            }
+            //x is negative so add in odd
+           //position like 1,3,5
+            else{
+                ans[neg]=x;
+                neg+=2;
             }
         }
-
         System.out.println(Arrays.toString(result));
     }
 }
